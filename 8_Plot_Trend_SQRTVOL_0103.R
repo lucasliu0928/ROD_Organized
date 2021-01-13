81a70ad711024f9f7111b85d148fe8bbd8ada6cb
JLLucas <39839798+jingliu0928@users.noreply.github.com>
2021-01-12 22:41:30 -0500

    Update 8_Plot_Trend_SQRTVOL_0103.R

--- 8_Plot_Trend_SQRTVOL_0103.R
+++ 8_Plot_Trend_SQRTVOL_0103.R
@@ -386,3 +386,113 @@ all_grp_dff_pvalues[signif_idxes,]
 write.csv(all_grp_dff_pvalues,paste0(plot_outdir,"grp_diff_test.csv"))
 
 
+#####################################################
+#Prediction task 3 violin plot
+#####################################################
+data_input <- updated_comb_df
+
+analysiis_label_df <- Label_df
+analysiis_label_df$New_Trend_Label <- NA
+analysiis_label_df$New_Trend_Label[which(analysiis_label_df$Changing_Label == "nonH_nonH")] <- 0 
+analysiis_label_df$New_Trend_Label[which(analysiis_label_df$Changing_Label == "nonH_H")] <- 1
+table(analysiis_label_df$New_Trend_Label) #34 15
+analysiis_label_df <- analysiis_label_df[-which(is.na(analysiis_label_df$New_Trend_Label)==T),]
+
+analysis_IDs <- rownames(analysiis_label_df) 
+
+#subset Ids has label
+Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
+data_input <- data_input[Id_idxes,]
+
+#Remove the co2 value at yr1 == 106 and -1
+ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
+data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
+analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
+
+
+#reoder to match and add label to data input
+data_input$New_Trend_Label <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),"New_Trend_Label"]
+
+# Basic violin plot for critical features
+critical_features_df <- read.csv(paste0(f_dir3, "/Exp1_important_fs.csv"),stringsAsFactors = F)
+critical_features <- critical_features_df$x
+
+plot_outdir <- paste0(f_dir3,"violin_plot/")
+dir.create(file.path(plot_outdir))
+for (f in 1:length(critical_features)){
+  plot_x <- "New_Trend_Label"
+  plot_y <-  critical_features[f]
+  
+  voilin_plot_data <- data_input[which(is.na(data_input$New_Trend_Label)==F),]
+  grp0_idex <- which(voilin_plot_data$New_Trend_Label==0)
+  grp1_idex <- which(voilin_plot_data$New_Trend_Label==1)
+  
+  voilin_plot_data$New_Trend_Label[grp0_idex] <- "(Grp 0) start non-high, \n stay non-high"
+  voilin_plot_data$New_Trend_Label[grp1_idex] <- "(Grp 1) start non-high, \n get higher"
+  
+  voilin_plot_data$New_Trend_Label <-as.factor(voilin_plot_data$New_Trend_Label)
+  
+  feature_unique_values_n <- length(levels(factor(voilin_plot_data[,plot_y])))
+  
+  png(paste0(plot_outdir,f,plot_y,"VS",plot_x,".png"))
+  if (feature_unique_values_n > 2){
+    p <- ggplot(voilin_plot_data, aes_string(x=plot_x, y=plot_y)) + 
+      geom_violin() +
+      geom_boxplot(width=0.1) + 
+      theme(axis.text=element_text(size=20),
+            axis.title=element_text(size=20,face="bold")) +
+      labs(y = plot_y,x = "")
+    print(p)
+    dev.off()
+  }else if (feature_unique_values_n == 2){ #if binary, use histogram
+    #Factorize the var, and remove NA for plot
+    plot_data <- voilin_plot_data
+    plot_data[,plot_y] <- factor(plot_data[,plot_y])
+    na_idxes <- which(is.na(plot_data[,plot_y])==T)
+    if (length(na_idxes) > 0){ #and remove NA for plot
+      plot_data <- plot_data[-na_idxes,]
+    }
+    
+    p <- ggplot(plot_data, aes_string(x=plot_y, fill = plot_x)) +
+      geom_histogram(alpha = 0.8,position="dodge",stat = "count") +
+      theme(legend.position='bottom',legend.title = element_blank()) +
+      scale_fill_manual(values=c("darkgreen", "darkred"))+
+      theme(axis.text=element_text(size=20),
+            axis.title=element_text(size=20),legend.text = element_text(size = 20))
+  }
+  print(p)
+  dev.off()
+}
+
+##########################################################################
+#normality test for each group
+##########################################################################
+#if p>0.05,it is norm
+#if p<0.05, it is not norm
+outcome_label <- "New_Trend_Label"
+norm_results <- list()
+for (f in 1:length(critical_features)){
+  curr_f <- critical_features[f]
+  norm_results[[f]] <- normality_test_func(data_input,outcome_label,curr_f)
+}
+all_norm_pvalues <- do.call(rbind,norm_results)
+#not normed distributed features
+NOT_normed_fs <- critical_features[which(all_norm_pvalues$p0<0.05 | all_norm_pvalues$p1<0.05)]
+normed_fs <- critical_features[which(all_norm_pvalues$p0>0.05 & all_norm_pvalues$p1>0.05)]
+
+##########################################################################
+###Grp diff test
+##########################################################################
+grp_diff_results <- list()
+for (f in 1:length(critical_features)){
+  curr_f <- critical_features[f]
+  grp_diff_results[[f]] <- grp_diff_test(data_input,curr_f,outcome_label,NOT_normed_fs,normed_fs)
+}
+all_grp_dff_pvalues <- do.call(rbind,grp_diff_results)
+
+signif_idxes <- which(all_grp_dff_pvalues$p_f0_less_f1 <=0.05 | all_grp_dff_pvalues$p_f0_greater_f1 <=0.05)
+all_grp_dff_pvalues[signif_idxes,]
+
+write.csv(all_grp_dff_pvalues,paste0(plot_outdir,"grp_diff_test.csv"))
+
+
