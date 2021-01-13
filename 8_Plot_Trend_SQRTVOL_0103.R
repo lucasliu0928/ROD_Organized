f0820b5ca1a41feb6ef656dade8b8fbf3bbe6f86
JLLucas <39839798+jingliu0928@users.noreply.github.com>
2021-01-12 22:03:10 -0500

    Update 8_Plot_Trend_SQRTVOL_0103.R

--- 8_Plot_Trend_SQRTVOL_0103.R
+++ 8_Plot_Trend_SQRTVOL_0103.R
@@ -7,9 +7,10 @@ library(openxlsx)
 
 
 ###Input and output directory
-in_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/ROD_Project/Intermediate_Data/0103_21/"
-f_dir1 <- "/Users/lucasliu/Desktop/DrChen_Projects/ROD_Project/Intermediate_Data/0103_21/out1/"
-f_dir2 <- "/Users/lucasliu/Desktop/DrChen_Projects/ROD_Project/Intermediate_Data/0103_21/out2/"
+in_dir <- "../Intermediate_Data/0112_21/"
+f_dir1 <- "../Intermediate_Data/0112_21/out1/"
+f_dir2 <- "../Intermediate_Data/0112_21/out2/"
+f_dir3 <- "../Intermediate_Data/0112_21/out3/"
 
 ###################################################
 ### Load feature data
@@ -86,9 +87,14 @@ analysis_IDs <- rownames(analysiis_label_df) #98
 Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
 data_input <- data_input[Id_idxes,]
 
+#Remove the co2 value at yr1 == 106 and -1
+ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
+data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
+analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
+
 #reoder to match and add label to data input
-data_input$Starting_CASCOREA_LABEL <- Label_df[match(rownames(Label_df),rownames(data_input)),"Starting_CASCOREA_LABEL"]
-data_input$Changing_Label <- Label_df[match(rownames(Label_df),rownames(data_input)),"Changing_Label"]
+data_input$Starting_CASCOREA_LABEL <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),"Starting_CASCOREA_LABEL"]
+data_input$Changing_Label <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),"Changing_Label"]
 table(data_input$Changing_Label)
 
 #new group for plot:
@@ -129,6 +135,11 @@ analysis_IDs <- rownames(analysiis_label_df) #107
 Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
 data_input <- data_input[Id_idxes,]
 
+#Remove the co2 value at yr1 == 106 and -1
+ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
+data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
+analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
+
 #reoder to match and add label to data input
 data_input$Starting_SqRtVOLUME_LABEL <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),"Starting_SqRtVOLUME_LABEL"]
 
@@ -137,27 +148,46 @@ critical_features_df <- read.csv(paste0(f_dir1, "/Exp1_important_fs.csv"),string
 critical_features <- critical_features_df$x
 
 plot_outdir <- paste0(f_dir1,"violin_plot/")
+dir.create(file.path(plot_outdir))
 for (f in 1:length(critical_features)){
   plot_x <- "Starting_SqRtVOLUME_LABEL"
   plot_y <-  critical_features[f]
-  
   
   voilin_plot_data <- data_input[which(is.na(data_input$Starting_SqRtVOLUME_LABEL)==F),]
   grp0_idex <- which(voilin_plot_data$Starting_SqRtVOLUME_LABEL==0)
   grp1_idex <- which(voilin_plot_data$Starting_SqRtVOLUME_LABEL==1)
   
-  voilin_plot_data$Starting_SqRtVOLUME_LABEL[grp0_idex] <- "(0) start non-high \n"
-  voilin_plot_data$Starting_SqRtVOLUME_LABEL[grp1_idex] <- "(1) start high \n"
+  voilin_plot_data$Starting_SqRtVOLUME_LABEL[grp0_idex] <- "(Grp 0) start non-high \n"
+  voilin_plot_data$Starting_SqRtVOLUME_LABEL[grp1_idex] <- "(Grp 1) start high \n"
   
   voilin_plot_data$Starting_SqRtVOLUME_LABEL <-as.factor(voilin_plot_data$Starting_SqRtVOLUME_LABEL)
   
+  feature_unique_values_n <- length(levels(factor(voilin_plot_data[,plot_y])))
+  
   png(paste0(plot_outdir,f,plot_y,"VS",plot_x,".png"))
-  p <- ggplot(voilin_plot_data, aes_string(x=plot_x, y=plot_y)) + 
-    geom_violin() +
-    geom_boxplot(width=0.1) + 
-    theme(axis.text=element_text(size=20),
-          axis.title=element_text(size=20,face="bold")) +
-    labs(y = plot_y,x = "")
+  if (feature_unique_values_n > 2){
+      p <- ggplot(voilin_plot_data, aes_string(x=plot_x, y=plot_y)) + 
+        geom_violin() +
+        geom_boxplot(width=0.1) + 
+        theme(axis.text=element_text(size=20),
+              axis.title=element_text(size=20,face="bold")) +
+        labs(y = plot_y,x = "")
+  }else if (feature_unique_values_n == 2){ #if binary, use histogram
+    #Factorize the var, 
+    plot_data <- voilin_plot_data
+    plot_data[,plot_y] <- factor(plot_data[,plot_y])
+    na_idxes <- which(is.na(plot_data[,plot_y])==T)
+    if (length(na_idxes) > 0){ #and remove NA for plot
+     plot_data <- plot_data[-na_idxes,]
+    }
+
+    p <- ggplot(plot_data, aes_string(x=plot_y, fill = plot_x)) +
+      geom_histogram(alpha = 0.8,position="dodge",stat = "count") +
+      theme(legend.position='bottom',legend.title = element_blank()) +
+      scale_fill_manual(values=c("darkgreen", "darkred"))+
+      theme(axis.text=element_text(size=20),
+            axis.title=element_text(size=20),legend.text = element_text(size = 20))
+  }
   print(p)
   dev.off()
 }
@@ -264,6 +294,12 @@ analysis_IDs <- rownames(analysiis_label_df) #92
 Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
 data_input <- data_input[Id_idxes,]
 
+#Remove the co2 value at yr1 == 106 and -1
+ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
+data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
+analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
+
+
 #reoder to match and add label to data input
 data_input$New_Trend_Label <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),"New_Trend_Label"]
 
@@ -272,6 +308,7 @@ critical_features_df <- read.csv(paste0(f_dir2, "/Exp1_important_fs.csv"),string
 critical_features <- critical_features_df$x
 
 plot_outdir <- paste0(f_dir2,"violin_plot/")
+dir.create(file.path(plot_outdir))
 for (f in 1:length(critical_features)){
   plot_x <- "New_Trend_Label"
   plot_y <-  critical_features[f]
@@ -280,18 +317,39 @@ for (f in 1:length(critical_features)){
   grp0_idex <- which(voilin_plot_data$New_Trend_Label==0)
   grp1_idex <- which(voilin_plot_data$New_Trend_Label==1)
   
-  voilin_plot_data$New_Trend_Label[grp0_idex] <- "(0) start non-high, \n stay non-high"
-  voilin_plot_data$New_Trend_Label[grp1_idex] <- "(1) start high, \n stay high"
+  voilin_plot_data$New_Trend_Label[grp0_idex] <- "(Grp 0) start non-high, \n stay non-high"
+  voilin_plot_data$New_Trend_Label[grp1_idex] <- "(Grp 1) start high, \n stay high"
   
   voilin_plot_data$New_Trend_Label <-as.factor(voilin_plot_data$New_Trend_Label)
   
+  feature_unique_values_n <- length(levels(factor(voilin_plot_data[,plot_y])))
+  
   png(paste0(plot_outdir,f,plot_y,"VS",plot_x,".png"))
-  p <- ggplot(voilin_plot_data, aes_string(x=plot_x, y=plot_y)) + 
-    geom_violin() +
-    geom_boxplot(width=0.1) + 
-    theme(axis.text=element_text(size=20),
-          axis.title=element_text(size=20,face="bold")) +
-    labs(y = plot_y,x = "")
+  if (feature_unique_values_n > 2){
+    p <- ggplot(voilin_plot_data, aes_string(x=plot_x, y=plot_y)) + 
+      geom_violin() +
+      geom_boxplot(width=0.1) + 
+      theme(axis.text=element_text(size=20),
+            axis.title=element_text(size=20,face="bold")) +
+      labs(y = plot_y,x = "")
+    print(p)
+    dev.off()
+  }else if (feature_unique_values_n == 2){ #if binary, use histogram
+    #Factorize the var, and remove NA for plot
+    plot_data <- voilin_plot_data
+    plot_data[,plot_y] <- factor(plot_data[,plot_y])
+    na_idxes <- which(is.na(plot_data[,plot_y])==T)
+    if (length(na_idxes) > 0){ #and remove NA for plot
+      plot_data <- plot_data[-na_idxes,]
+    }
+    
+    p <- ggplot(plot_data, aes_string(x=plot_y, fill = plot_x)) +
+      geom_histogram(alpha = 0.8,position="dodge",stat = "count") +
+      theme(legend.position='bottom',legend.title = element_blank()) +
+      scale_fill_manual(values=c("darkgreen", "darkred"))+
+      theme(axis.text=element_text(size=20),
+            axis.title=element_text(size=20),legend.text = element_text(size = 20))
+  }
   print(p)
   dev.off()
 }
