ed611352860a1f4adf379cc3a7c48cea82180047
JLLucas <39839798+jingliu0928@users.noreply.github.com>
2021-01-20 04:45:40 -0500

    Update 4_Prediction_SqrtVol.R

--- 4_Prediction_SqrtVol.R
+++ 4_Prediction_SqrtVol.R
@@ -6,10 +6,10 @@ library(ggplot2)
 library(xgboost)
 
 ###Input and output directory
-in_dir <- "../Intermediate_Data/0112_21/"
-out_dir <- "../Intermediate_Data/0112_21/out1/"
-out_dir2 <- "../Intermediate_Data/0112_21/out2/"
-out_dir3 <-  "../Intermediate_Data/0112_21/out3/"
+in_dir <- "../Intermediate_Data/0117_21/"
+out_dir <- "../Intermediate_Data/0117_21/out1/"
+out_dir2 <- "../Intermediate_Data/0117_21/out2/"
+out_dir3 <-  "../Intermediate_Data/0117_21/out3/"
 
 dir.create(file.path(out_dir))
 dir.create(file.path(out_dir2))
@@ -27,12 +27,12 @@ final_df <- final_df[,-1]
 final_delta_df <- read.csv(paste0(in_dir,"data_Diff_POSandNeg.csv"), stringsAsFactors = F)
 rownames(final_delta_df) <- final_delta_df$X
 final_delta_df <- final_delta_df[,-1]
-
 reorder_tomatch_df <- final_delta_df[match(rownames(final_delta_df), rownames(final_df)),]
+
+#3. Combine stationary feature and feature difference.
 comb_df <- cbind(final_df, reorder_tomatch_df)
 
-#3remove outcome related features SqRtVOLUMEA,CASCOREA(BL,yr1,yr2,Diff0_2,Diff1_2,Diff0_1)
-#remove orignal label columns
+#4. remove outcome related features SqRtVOLUMEA,CASCOREA(BL,yr1,yr2,Diff0_2,Diff1_2,Diff0_1) #remove orignal label columns
 original_stalabel_names <- c("CASCOREA_bl" , "CASCORE1A_yr1", "CASCORE2A_yr2", 
                              "SqRtVOLUMEA_bl", "SqRtVOLUME1A_yr1", "SqRtVOLUME2A_yr2")
 original_trendlabel_names <- c("CASCOREA_Diff0_1" , "CASCOREA_Diff1_2", "CASCOREA_Diff0_2", 
@@ -46,25 +46,27 @@ pos_neg_trendlabel_names <- c("CASCOREA_Diff0_1_pos" , "CASCOREA_Diff0_1_neg",
 
 
 updated_comb_df <- comb_df[, -which(colnames(comb_df) %in% c(original_stalabel_names,original_trendlabel_names,pos_neg_trendlabel_names))]
-updated_delta_df <- final_delta_df[, -which(colnames(final_delta_df) %in% c(original_trendlabel_names,pos_neg_trendlabel_names))]
-updated_static_df <- final_df[, -which(colnames(final_df) %in% c(original_stalabel_names))]
 
 
-#get feaures
-Final_bl_features <- colnames(updated_comb_df)[which(grepl("bl",colnames(updated_comb_df))==T)]
-Final_yr1_features <- colnames(updated_comb_df)[which(grepl("yr1",colnames(updated_comb_df))==T)]
-Final_yr2_features <- colnames(updated_comb_df)[which(grepl("yr2",colnames(updated_comb_df))==T)]
-Diff0_1_features <- colnames(updated_comb_df)[which(grepl("Diff0_1",colnames(updated_comb_df))==T)]
-Diff1_2_features <- colnames(updated_comb_df)[which(grepl("Diff1_2",colnames(updated_comb_df))==T)]
-Diff0_2_features <- colnames(updated_comb_df)[which(grepl("Diff0_2",colnames(updated_comb_df))==T)]
+#5. get feature names
+Final_bl_features <- colnames(updated_comb_df)[which(grepl("bl",colnames(updated_comb_df))==T)] #68 #4 feature has no yr1 and yr2 values
+Final_yr1_features <- colnames(updated_comb_df)[which(grepl("yr1",colnames(updated_comb_df))==T)] #64
+Final_yr2_features <- colnames(updated_comb_df)[which(grepl("yr2",colnames(updated_comb_df))==T)] #64
+Diff0_1_features <- colnames(updated_comb_df)[which(grepl("Diff0_1",colnames(updated_comb_df))==T)] #128
+Diff1_2_features <- colnames(updated_comb_df)[which(grepl("Diff1_2",colnames(updated_comb_df))==T)] #128
+Diff0_2_features <- colnames(updated_comb_df)[which(grepl("Diff0_2",colnames(updated_comb_df))==T)] #128
 
 
 
 ##########################################################################
 ####I. Load all catogrized labels
-###A. Get catogrized label of SqRtVOLUME at bl,yr1 and yr2 
+###A. Get catogrized label of starting value of SqRtVOLUME
 #  A1. start Non-high risk : SqRtVOLUME < 19.1 (0)
 #  A2. start High Risk:      SqRtVOLUME >= 19.1 (1)
+###B. Changing label
+###   1.	Start high, stay high
+###   2.  start non-high, stay non-high
+###   3.  start non-high, get higher 
 ##########################################################################
 Label_df <- read.csv(paste0(in_dir,"Outcome_SqRtVOLUME.csv"), stringsAsFactors = F)
 rownames(Label_df) <- Label_df$X
@@ -73,72 +75,19 @@ Label_df <- Label_df[,-1]
 
 ##########################################################################
 ###Prediction 1: start non high vs high
-###
 ##########################################################################
-analysiis_label_df <- Label_df
-analysiis_label_df$Starting_SqRtVOLUME_LABEL[which(analysiis_label_df$Starting_SqRtVOLUME_LABEL == "start_nonH")] <- 0 
-analysiis_label_df$Starting_SqRtVOLUME_LABEL[which(analysiis_label_df$Starting_SqRtVOLUME_LABEL == "start_H")] <- 1
-table(analysiis_label_df$Starting_SqRtVOLUME_LABEL) #49 58
-
-data_input <- updated_comb_df
+#Add corresponding label to the data 
 label_col_name <- "Starting_SqRtVOLUME_LABEL"
-analysis_IDs <- rownames(analysiis_label_df) #107
+grp0_name <- "start_nonH"
+grp1_name <- "start_H"
+data_input <- experiment_setup_func(updated_comb_df, Label_df, label_col_name, grp0_name,grp1_name)
 
-#subset Ids has label
-Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
-data_input <- data_input[Id_idxes,]
+##generating list of features and labels for each experiments
+label_col_seq <- c(rep(label_col_name,3))
+experiment_features_names_list <- c("bl,yr1,yr2","Diff0_1_and_Diff1_2","ALL")
+initial_features_list <- get_feature_list_func(experiment_features_names_list,Final_bl_features,Final_yr1_features,Final_yr2_features, Diff0_1_features,Diff1_2_features)
 
-#Output the input data for checking outliers
-#checkoutlier_df <- final_df[which(rownames(final_df) %in% analysis_IDs),]
-#write.csv(checkoutlier_df,"/Users/lucasliu/Desktop/ROD_data_input.csv")
-
-#'@NOTE: Nothing updated in this file Lucasfile2_CorrectedCO2.xlsx
-# #Updated Manually corrected outlier values of CO2
-# corrected_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/ROD_Project/Data/Lucasfile2_CorrectedCO2.xlsx"
-# outlier_corrected_df <- read.xlsx(corrected_dir, sheet = 1)
-# outlier_corrected_df <- outlier_corrected_df[match(rownames(data_input),outlier_corrected_df$X1),]
-# 
-# check <- cbind(outlier_corrected_df[,c("X1","Dialysate_bl","CO2_bl")],data_input[,c("Dialysate_bl","CO2_bl")])
-# colnames(check)[2:3] <- c("Dialysate_bl_updated","CO2_bl_updated")
-
-#Remove the co2 value at yr1 == 106 and -1
-ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
-data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
-analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
-
-#reoder to match and add label to data input
-data_input$Starting_SqRtVOLUME_LABEL <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),label_col_name]
-
-##generating list of features for each  experiment
-label_col_seq <- c(rep("Starting_SqRtVOLUME_LABEL",3))
-initial_features_names_list <- c("bl,yr1,yr2","Diff0_1_and_Diff1_2","ALL")
-
-initial_features_list <- list()
-for (i in 1:length(initial_features_names_list)){
-  curr_feature_list_names <- initial_features_names_list[i]
-  
-  if (curr_feature_list_names == "bl"){
-    initial_features_list[[i]] <- Final_bl_features
-  }else if (curr_feature_list_names == "bl,yr1"){
-    initial_features_list[[i]] <- c(Final_bl_features,Final_yr1_features)
-  }else if (curr_feature_list_names == "bl,yr1,yr2"){
-    initial_features_list[[i]] <- c(Final_bl_features,Final_yr1_features,Final_yr2_features)
-  }else if (curr_feature_list_names == "Diff0_1"){
-    initial_features_list[[i]]  <- Diff0_1_features
-  }else if (curr_feature_list_names == "Diff1_2"){
-    initial_features_list[[i]]  <- Diff1_2_features
-  }else if (curr_feature_list_names == "Diff0_2"){
-    initial_features_list[[i]]  <- Diff0_2_features
-  }else if (curr_feature_list_names == "Diff0_1_and_Diff1_2"){
-    initial_features_list[[i]]  <- c(Diff0_1_features,Diff1_2_features)
-  }else if (curr_feature_list_names == "ALL"){
-    initial_features_list[[i]]  <- c(Final_bl_features,Final_yr1_features,Final_yr2_features,
-                                     Diff0_1_features,Diff1_2_features)
-  }
-  
-}
-
-
+##Run exoeriments predict label in label_col_seq, using features in experiment_features_names_list
 res <- experiment_func(label_col_seq,initial_features_list,data_input,out_dir)
 Final_perf <- res[[1]]
 initail_fs <- res[[2]]
@@ -148,11 +97,11 @@ all_labeltb <- do.call(rbind,label_table)
 top_features_names_list1 <- res[[5]]
 for(i in 1:length(top_features_names_list1)){
   curr_important_fs <- top_features_names_list1[[i]]
-  write.csv(curr_important_fs,paste0(out_dir,"/Exp",i,"_important_fs",".csv"))
+  write.csv(curr_important_fs,paste0(out_dir,"/FeatureSet",i,"_important_fs",".csv"))
   
 }
 
-Final_perf$Features <- initial_features_names_list
+Final_perf$Features <- experiment_features_names_list
 Final_perf$Outcome <- label_col_seq
 Final_perf$initail_fs <- initail_fs
 Final_perf$Final_fs <- Final_fs
@@ -163,61 +112,22 @@ write.csv(Final_perf,paste0(out_dir,"/Perf_tb.csv"))
 ##########################################################################
 ###Prediction 2:  Start High, stays high vs. Start non-high, stay non-high
 ##########################################################################
-analysiis_label_df <- Label_df
-analysiis_label_df$New_Trend_Label <- NA
-analysiis_label_df$New_Trend_Label[which(analysiis_label_df$Changing_Label == "nonH_nonH")] <- 0 
-analysiis_label_df$New_Trend_Label[which(analysiis_label_df$Changing_Label == "H_H")] <- 1
-table(analysiis_label_df$New_Trend_Label) #34 58
-analysiis_label_df <- analysiis_label_df[-which(is.na(analysiis_label_df$New_Trend_Label)==T),]
+#first remvoe nonH_H grp
+pts_toremove <- which(Label_df[,"Changing_Label"] == "nonH_H") 
+updated_label_df <- Label_df[-pts_toremove,]
 
-data_input <- updated_comb_df
-label_col_name <- "New_Trend_Label"
-analysis_IDs <- rownames(analysiis_label_df) #92
-length(analysis_IDs)
+#Add corresponding label to the data 
+label_col_name <- "Changing_Label"
+grp0_name <- "nonH_nonH"
+grp1_name <- "H_H"
+data_input <- experiment_setup_func(updated_comb_df, updated_label_df, label_col_name, grp0_name,grp1_name)
 
-#subset Ids has label
-Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
-data_input <- data_input[Id_idxes,]
+##generating list of features and labels for each experiments
+label_col_seq <- c(rep(label_col_name,3))
+experiment_features_names_list <- c("bl,yr1,yr2","Diff0_1_and_Diff1_2","ALL")
+initial_features_list <- get_feature_list_func(experiment_features_names_list,Final_bl_features,Final_yr1_features,Final_yr2_features, Diff0_1_features,Diff1_2_features)
 
-#Remove the co2 value at yr1 == 106 and -1
-ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
-data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
-analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
-
-#reoder to match and add label to data input
-data_input$New_Trend_Label <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),label_col_name]
-
-
-##generating list of features for each  experiment
-label_col_seq <- c(rep("New_Trend_Label",3))
-initial_features_names_list <- c("bl,yr1,yr2","Diff0_1_and_Diff1_2","ALL")
-
-initial_features_list <- list()
-for (i in 1:length(initial_features_names_list)){
-  curr_feature_list_names <- initial_features_names_list[i]
-  
-  if (curr_feature_list_names == "bl"){
-    initial_features_list[[i]] <- Final_bl_features
-  }else if (curr_feature_list_names == "bl,yr1"){
-    initial_features_list[[i]] <- c(Final_bl_features,Final_yr1_features)
-  }else if (curr_feature_list_names == "bl,yr1,yr2"){
-    initial_features_list[[i]] <- c(Final_bl_features,Final_yr1_features,Final_yr2_features)
-  }else if (curr_feature_list_names == "Diff0_1"){
-    initial_features_list[[i]]  <- Diff0_1_features
-  }else if (curr_feature_list_names == "Diff1_2"){
-    initial_features_list[[i]]  <- Diff1_2_features
-  }else if (curr_feature_list_names == "Diff0_2"){
-    initial_features_list[[i]]  <- Diff0_2_features
-  }else if (curr_feature_list_names == "Diff0_1_and_Diff1_2"){
-    initial_features_list[[i]]  <- c(Diff0_1_features,Diff1_2_features)
-  }else if (curr_feature_list_names == "ALL"){
-    initial_features_list[[i]]  <- c(Final_bl_features,Final_yr1_features,Final_yr2_features,
-                                     Diff0_1_features,Diff1_2_features)
-  }
-  
-}
-
-
+##Run exoeriments predict label in label_col_seq, using features in experiment_features_names_list
 res <- experiment_func(label_col_seq,initial_features_list,data_input,out_dir2)
 Final_perf <- res[[1]]
 initail_fs <- res[[2]]
@@ -227,11 +137,11 @@ all_labeltb <- do.call(rbind,label_table)
 top_features_names_list2 <- res[[5]]
 for(i in 1:length(top_features_names_list2)){
   curr_important_fs <- top_features_names_list2[[i]]
-  write.csv(curr_important_fs,paste0(out_dir2,"/Exp",i,"_important_fs",".csv"))
+  write.csv(curr_important_fs,paste0(out_dir2,"/FeatureSet",i,"_important_fs",".csv"))
   
 }
 
-Final_perf$Features <- initial_features_names_list
+Final_perf$Features <- experiment_features_names_list
 Final_perf$Outcome <- label_col_seq
 Final_perf$initail_fs <- initail_fs
 Final_perf$Final_fs <- Final_fs
@@ -241,60 +151,20 @@ write.csv(Final_perf,paste0(out_dir2,"/Perf_tb.csv"))
 ##########################################################################
 ###Prediction 3:  Start non-high, get higher vs. Start non-high, stays non-high 
 ##########################################################################
-analysiis_label_df <- Label_df
-analysiis_label_df$New_Trend_Label <- NA
-analysiis_label_df$New_Trend_Label[which(analysiis_label_df$Changing_Label == "nonH_nonH")] <- 0 
-analysiis_label_df$New_Trend_Label[which(analysiis_label_df$Changing_Label == "nonH_H")] <- 1
-table(analysiis_label_df$New_Trend_Label)  #34 15
-analysiis_label_df <- analysiis_label_df[-which(is.na(analysiis_label_df$New_Trend_Label)==T),]
+#first remvoe H_H grp
+pts_toremove <- which(Label_df[,"Changing_Label"] == "H_H") 
+updated_label_df <- Label_df[-pts_toremove,]
 
-data_input <- updated_comb_df
-label_col_name <- "New_Trend_Label"
-analysis_IDs <- rownames(analysiis_label_df) #49
-length(analysis_IDs)
+#Add corresponding label to the data 
+label_col_name <- "Changing_Label"
+grp0_name <- "nonH_nonH"
+grp1_name <- "nonH_H"
+data_input <- experiment_setup_func(updated_comb_df, updated_label_df, label_col_name, grp0_name,grp1_name)
 
-#subset Ids has label
-Id_idxes <- which(rownames(data_input) %in% analysis_IDs)
-data_input <- data_input[Id_idxes,]
-
-#Remove the co2 value at yr1 == 106 and -1
-ptsID_to_remove <- rownames(data_input)[which(data_input[,"CO21_yr1"] %in% c(106,-1))]
-data_input <- data_input[-which(rownames(data_input) %in% ptsID_to_remove),]
-analysiis_label_df <- analysiis_label_df[-which(rownames(analysiis_label_df) %in% ptsID_to_remove),]
-
-#reoder to match and add label to data input
-data_input$New_Trend_Label <- analysiis_label_df[match(rownames(analysiis_label_df),rownames(data_input)),label_col_name]
-table(data_input$New_Trend_Label)
-
-##generating list of features for each  experiment
-label_col_seq <- c(rep("New_Trend_Label",3))
-initial_features_names_list <- c("bl,yr1,yr2","Diff0_1_and_Diff1_2","ALL")
-
-initial_features_list <- list()
-for (i in 1:length(initial_features_names_list)){
-  curr_feature_list_names <- initial_features_names_list[i]
-  
-  if (curr_feature_list_names == "bl"){
-    initial_features_list[[i]] <- Final_bl_features
-  }else if (curr_feature_list_names == "bl,yr1"){
-    initial_features_list[[i]] <- c(Final_bl_features,Final_yr1_features)
-  }else if (curr_feature_list_names == "bl,yr1,yr2"){
-    initial_features_list[[i]] <- c(Final_bl_features,Final_yr1_features,Final_yr2_features)
-  }else if (curr_feature_list_names == "Diff0_1"){
-    initial_features_list[[i]]  <- Diff0_1_features
-  }else if (curr_feature_list_names == "Diff1_2"){
-    initial_features_list[[i]]  <- Diff1_2_features
-  }else if (curr_feature_list_names == "Diff0_2"){
-    initial_features_list[[i]]  <- Diff0_2_features
-  }else if (curr_feature_list_names == "Diff0_1_and_Diff1_2"){
-    initial_features_list[[i]]  <- c(Diff0_1_features,Diff1_2_features)
-  }else if (curr_feature_list_names == "ALL"){
-    initial_features_list[[i]]  <- c(Final_bl_features,Final_yr1_features,Final_yr2_features,
-                                     Diff0_1_features,Diff1_2_features)
-  }
-  
-}
-
+##generating list of features and labels for each experiments
+label_col_seq <- c(rep(label_col_name,3))
+experiment_features_names_list <- c("bl,yr1,yr2","Diff0_1_and_Diff1_2","ALL")
+initial_features_list <- get_feature_list_func(experiment_features_names_list,Final_bl_features,Final_yr1_features,Final_yr2_features, Diff0_1_features,Diff1_2_features)
 
 res <- experiment_func(label_col_seq,initial_features_list,data_input,out_dir3)
 Final_perf <- res[[1]]
@@ -305,11 +175,11 @@ all_labeltb <- do.call(rbind,label_table)
 top_features_names_list2 <- res[[5]]
 for(i in 1:length(top_features_names_list2)){
   curr_important_fs <- top_features_names_list2[[i]]
-  write.csv(curr_important_fs,paste0(out_dir3,"/Exp",i,"_important_fs",".csv"))
+  write.csv(curr_important_fs,paste0(out_dir3,"/FeatureSet",i,"_important_fs",".csv"))
   
 }
 
-Final_perf$Features <- initial_features_names_list
+Final_perf$Features <- experiment_features_names_list
 Final_perf$Outcome <- label_col_seq
 Final_perf$initail_fs <- initail_fs
 Final_perf$Final_fs <- Final_fs
